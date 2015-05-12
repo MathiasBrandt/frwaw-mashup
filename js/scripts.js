6e90a8b50c34fc09d46ed50b98d75ed97b95a957
Mathias Brandt <maini123@gmail.com>
2015-05-12 22:25:53 +0200

    Added bootstrap, styling and some tweaks

--- js/scripts.js
+++ js/scripts.js
@@ -5,7 +5,7 @@ function searchFlickr() {
     var searchTerm = document.getElementById('flickr.searchTerm').value;
 
     doSearch(searchTerm).done(function(data) {
-        $('#resultCount').append(' ' + data.photos.total);
+        $('#resultCount').html('Results: ' + data.photos.total);
 
         data.photos.photo.forEach(function(photo) {
             // TODO: instead of using appendTo() multiple times, somehow concatenate the results together and perform one big appendTo()
@@ -28,23 +28,22 @@ function doSearch(searchTerm) {
             'api_key' : apiKey,
             'format' : 'json',
             'tags' : searchTerm,
-            'per_page' : 3,
+            'per_page' : 100,
             'nojsoncallback' : 1
         }
     });
 }
 
 function createImage(photo) {
-    var container = $('<div/>', {
-        class: 'flickrImage'
-    });
+    var container = $('<div/>', { class: 'col-xs-3 flickrImage' });
 
     var title = $('<div/>', {
-        class: 'title',
+        class: 'title wordwrap col-xs-12',
         text: photo.title
     });
 
     var image = jQuery('<img/>', {
+        class: 'col-xs-12',
         src: 'https://farm' + photo.farm + '.staticflickr.com/' + photo.server + '/' + photo.id + '_' + photo.secret + '.jpg'
     });
 
